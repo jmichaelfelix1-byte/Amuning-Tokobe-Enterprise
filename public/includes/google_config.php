4addeb1c6eaaef3b5d7e1b9511e866f9af3fc6b4
Xeeennn <91773859+Xeeennn@users.noreply.github.com>
2026-04-11 10:42:04 +0800

    Change Google redirect URI to hardcoded value
    Updated Google OAuth configuration to use a hardcoded redirect URI for deployment.

--- public/includes/google_config.php
+++ public/includes/google_config.php
@@ -3,13 +3,8 @@
 define('GOOGLE_CLIENT_ID', '765395085085-4qok9ltliuvnuqfus56of2jj9592tb6o.apps.googleusercontent.com');
 define('GOOGLE_CLIENT_SECRET', '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');
 
-// Dynamic redirect URI - uses current domain instead of localhost
-$protocol = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') ? 'https://' : 'http://';
-$domain = $_SERVER['HTTP_HOST'];
-define('GOOGLE_REDIRECT_URI', $protocol . $domain . '/Amuning/public/google_auth.php');
-
-// DEBUG: See what URI is being generated
-error_log('Generated Redirect URI: ' . GOOGLE_REDIRECT_URI);
+// Hardcoded for Render.com deployment
+define('GOOGLE_REDIRECT_URI', 'https://amuning-tokobe-enterprise.onrender.com/Amuning/public/google_auth.php');
 
 // Include Google API Client
 require_once __DIR__ . '../../../google_login/vendor/autoload.php';
