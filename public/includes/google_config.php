2a323bad7d1ed8cc61872d5f3e720372af485806
Xeeennn <91773859+Xeeennn@users.noreply.github.com>
2026-04-11 10:49:33 +0800

    Update google_config.php

--- public/includes/google_config.php
+++ public/includes/google_config.php
@@ -3,8 +3,11 @@
 define('GOOGLE_CLIENT_ID', '765395085085-4qok9ltliuvnuqfus56of2jj9592tb6o.apps.googleusercontent.com');
 define('GOOGLE_CLIENT_SECRET', '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');
 
-// Hardcoded for Render.com deployment
-define('GOOGLE_REDIRECT_URI', 'https://amuning-tokobe-enterprise.onrender.com/Amuning/public/google_auth.php');
+// Use correct path for deployed environment
+// In Docker, the document root is /var/www/html/public, so we don't need /Amuning
+$protocol = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') ? 'https://' : 'http://';
+$domain = $_SERVER['HTTP_HOST'];
+define('GOOGLE_REDIRECT_URI', $protocol . $domain . '/google_auth.php');
 
 // Include Google API Client
 require_once __DIR__ . '../../../google_login/vendor/autoload.php';
