2d15ba6f487d2b0dd3a37ea7c34c03070d58c9a1
Xeeennn <91773859+Xeeennn@users.noreply.github.com>
2026-04-11 09:23:33 +0800

    Make Google redirect URI dynamic
    Updated the Google redirect URI to be dynamic based on the current domain.

--- public/includes/google_config.php
+++ public/includes/google_config.php
@@ -2,7 +2,11 @@
 // Google OAuth Configuration
 define('GOOGLE_CLIENT_ID', '497276011851-0289o7mu4tocr5k2frpes1955rpkedc8.apps.googleusercontent.com');
 define('GOOGLE_CLIENT_SECRET', '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');
-define('GOOGLE_REDIRECT_URI', 'http://localhost/Amuning/public/google_auth.php');
+
+// Dynamic redirect URI - uses current domain instead of localhost
+$protocol = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') ? 'https://' : 'http://';
+$domain = $_SERVER['HTTP_HOST'];
+define('GOOGLE_REDIRECT_URI', $protocol . $domain . '/Amuning/public/google_auth.php');
 
 // Include Google API Client
 require_once __DIR__ . '../../../google_login/vendor/autoload.php';
